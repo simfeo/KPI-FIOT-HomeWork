4c210a766c68b38401963fe89fa537611e110a83
Smbat Makiyan <simfeo@yandex.ru>
2018-06-02 06:51:57 +0300

    More pretty fields

--- sem4/PhoneBook/PhoneBook/Models/PhoneCard.cs
+++ sem4/PhoneBook/PhoneBook/Models/PhoneCard.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.Data.Entity;
 
 namespace PhoneBook.Models
@@ -6,10 +7,17 @@ namespace PhoneBook.Models
     public class PhoneCard
     {
         public int ID { get; set; }
+
+        [Display(Name = "First name")]
         public string FirstName { get; set; }
+        [Display(Name = "First name")]
         public string LastName { get; set; }
+        [Display(Name = "Birth date")]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime BirthDate { get; set; }
         public string Address { get; set; }
+        [Display(Name = "Phone Number")]
         public string PhoneNumber { get; set; }
     }
 
