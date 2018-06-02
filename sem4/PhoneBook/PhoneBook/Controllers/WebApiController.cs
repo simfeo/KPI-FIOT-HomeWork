45a113c5bd55120ece0ffc3e9f070c79e542da46
Smbat Makiyan <simfeo@yandex.ru>
2018-06-02 04:57:55 +0300

    Web api forcs well http://localhost:7106/static/ApiTest.html http://localhost:7106/api/webapi/

---
+++ sem4/PhoneBook/PhoneBook/Controllers/WebApiController.cs
@@ -0,0 +1,39 @@
+﻿using PhoneBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace PhoneBook.Controllers
+{
+    public class WebApiController : ApiController
+    {
+        PhoneCard[] phoneCards = new PhoneCard[]
+            {
+                new PhoneCard {ID = 1, FirstName = "Dummy", LastName = "Dummy2", BirthDate = new DateTime(2008, 3, 9, 16, 5, 7, 123), Address = "Somwhere beyound the see", PhoneNumber="+123456789" },
+                new PhoneCard {ID = 2, FirstName = "Duck", LastName = "McLaren", BirthDate = new DateTime(1989, 3, 9, 16, 5, 7, 123), Address = "Meme land, 13, fl. 123", PhoneNumber="+5454654" },
+                new PhoneCard {ID = 3, FirstName = "Donald", LastName = "McDonald", BirthDate = new DateTime(2000, 3, 9, 16, 5, 7, 123), Address = "Hell, second ring.", PhoneNumber="+7878787" }
+            };
+
+
+
+        public IEnumerable<PhoneCard> GetAllProducts()
+        {
+            return phoneCards;
+        }
+
+        public IHttpActionResult GetCard(int id)
+        {
+            var product = phoneCards.FirstOrDefault((p) => p.ID == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            return Ok(product);
+        }
+    }
+}
+
+
