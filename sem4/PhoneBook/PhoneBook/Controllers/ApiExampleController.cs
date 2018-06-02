7d889a6674bb37ae8339c574caeef562deb84af1
Smbat Makiyan <simfeo@yandex.ru>
2018-06-02 06:43:28 +0300

    New bootstrap

---
+++ sem4/PhoneBook/PhoneBook/Controllers/ApiExampleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PhoneBook.Controllers
+{
+    public class ApiExampleController : Controller
+    {
+        // GET: ApiExample
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
